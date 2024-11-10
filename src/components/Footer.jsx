c8d216a46d26a0c8524229d65c3ea67171f18541
Patrick MS <patrickms567@gmail.com>
2024-11-10 10:53:55 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is import
 import "./../style/footer.css"
 
 const Footer = ({ isDarkMode }) => (
-  <footer className={`footer text-center py-3 ${isDarkMode ? "bg-dark text-white" : "bg-light text-dark"}`}>
-    <div className="container">
-      <p>&copy; 2024 Patrick. All rights reserved.</p>
-    </div>
-    <BackToTopButton isDarkMode={isDarkMode} /> {/* Pass the isDarkMode prop */}
-  </footer>
+  <footer
+  className={`footer text-center py-3 ${isDarkMode ? "bg-dark text-white" : "bg-light text-dark"}`}
+>
+  <div className="container">
+    <p>
+      &copy; 2024{" "}
+      <span style={{ color: isDarkMode ? "yellow" : "inherit", fontWeight: isDarkMode ? "bold" : "normal" }}>
+        {isDarkMode ? "Patrick Batman" : "Patrick"}
+      </span>
+      . All rights reserved.
+    </p>
+  </div>
+  <BackToTopButton isDarkMode={isDarkMode} /> {/* Pass the isDarkMode prop */}
+</footer>
 );
 
 const BackToTopButton = ({ isDarkMode }) => { // Accept isDarkMode as a prop
