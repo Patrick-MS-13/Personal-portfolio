a5ca455fb65cf4ddea229c9336ad2c75761be227
Patrick MS <patrickms567@gmail.com>
2025-02-22 22:18:06 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import nightmode from "./../img/night-mode.png";
+// import nightmode from "./../img/night-mode.png";
 import "./../style/navbar.css";
 import "./../style/darkmode.css";
 
