5f55a159d567823afa7bd45d6a4bf1dcad12a880
Solomon Rojer <solomonrojer@Solomons-MacBook-Air.local>
2025-03-31 13:52:03 +0530

    Updated CSS for responsive navbar and buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
 import "./../style/footer.css"
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const MyNavbar = ({ isDarkMode, toggleDarkMode }) => {
               <span style={{ height: "22px", paddingTop: "3px", fontSize: "11px" }}>BatMan</span>            </Button>
           ) : (
             <Button variant="light" onClick={toggleDarkMode} className="button" style={{ width: "90px", height: "30px" }}>
-              <p style={{ width: "53", height: "23px"}}>call the batman</p>
+              <p style={{ width: "150", height: "22px"}}>call the batman</p>
             </Button>
           )}
         </div>
